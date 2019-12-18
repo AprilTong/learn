57f488c2695a5ebf2c666e06f48bd556823bdbb3
tongzhirong <tongzhirong@bizvane.cn>
2019-12-18 18:12:23 +0800

    12。18

--- learn-day/algorithm/1.js
+++ learn-day/algorithm/1.js
@@ -58,7 +58,7 @@ function findTargetIndex(arr, target) {
     let diff = target - arr[i]
     result[arr[i]] = i
     if (diff in result) {
-    console.timeEnd('bb')
+      console.timeEnd('bb')
       return [i, result[diff]]
     }
   }
@@ -66,3 +66,28 @@ function findTargetIndex(arr, target) {
 
 // 3. 统计一个字符串出现最多的字母
 // 4. 生成一个随机长度的字符串
+
+function a() {
+  this.b = 3
+}
+a.prototype.b = 7
+var t = new a()
+var b = 2
+a()
+console.log(t.b) // 3
+console.log(b) // 3
+
+// 利用reduce实现数组的map方法
+Array.prototype.myMap2 = function(fn) {
+  // let arr = this
+  return this.reduce((acc, cur, index) => {
+    acc.push(fn(cur, index, this))
+    return acc
+  }, [])
+}
+
+// es5的继承和es6的class的继承有什么区别
+// 1. ES6的继承实现在于使用super关键字调用父类，ES5是通过call或者apply回调方法调用父类
+// 2. ES6的继承子类可以直接通过 __proto__ 寻址到父类，ES5的__proto__指向Function.prototype
+// 3. ES6 的继承先生成父类实例，再调用子类的构造函数修饰父类实例，ES5 的继承先生成了子类实例，再调用父类的构造函数修饰子类实例
+// 4. ES6类的内部所有定义的方法，都是不可枚举的
